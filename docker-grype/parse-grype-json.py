8d82fab3820ef1707a5ec974faf4fc9139c82010
Ben Dalling <b.dalling@locp.co.uk>
2022-09-22 21:10:09 +0100

    fix: dev: Sort out default value for file name III.

--- docker-grype/parse-grype-json.py
+++ docker-grype/parse-grype-json.py
@@ -37,7 +37,7 @@ def main(filename: str = None) -> int:
 
 
 if __name__ == '__main__':
-    if len(sys.argv):
+    if len(sys.argv) > 1:
         sys.exit(main(sys.argv[1]))
     else:
         sys.exit(main())
